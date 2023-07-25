80ad35920755740bcd58f1039988d1c85bd84622
ed-nesma02 <shkurko26081978@gmail.com>
2023-07-26 01:04:00 +0300

    cart delete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/"; //https://freshy-bar-api.glitch.me/
+const API_URL = "https://freshy-bar-api.glitch.me/"; //https://freshy-bar-api.glitch.me/
 
 const price = {
   Клубника: 60,
@@ -22,17 +22,20 @@ const cartDataControl = {
     item.idls = Math.random().toString(36).substring(2, 8);
     cartData.push(item);
     localStorage.setItem("FreshyBarCart", JSON.stringify(cartData));
+    initCountCart();
   },
   remove(idls) {
     const cartData = this.get();
     const index = cartData.findIndex((item) => item.idls === idls);
     if (index !== -1) {
-      cartData.splice(index, -1);
+      cartData.splice(index, 1);
     }
     localStorage.setItem("FreshyBarCart", JSON.stringify(cartData));
+    initCountCart();
   },
   clear() {
     localStorage.removeItem("FreshyBarCart");
+    initCountCart();
   },
 };
 
@@ -293,7 +296,18 @@ const createCartItem = (item) => {
   </div>
   <button class="order__item-delete" aria-label="Удалить коктейл из корзины" data-idls="${
     item.idls
-  }"></button>
+  }">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_26_867)">
+    <path d="M12.9401 11.9821L18.4667 6.47024C18.5759 6.34304 18.633 6.17943 18.6265 6.0121C18.6201 5.84476 18.5505 5.68603 18.4318 5.56762C18.313 5.4492 18.1539 5.37984 17.9861 5.37337C17.8183 5.36691 17.6543 5.42383 17.5267 5.53275L12.0001 11.0446L6.4734 5.5261C6.34786 5.4009 6.1776 5.33057 6.00006 5.33057C5.82253 5.33057 5.65227 5.4009 5.52673 5.5261C5.40119 5.6513 5.33067 5.82111 5.33067 5.99817C5.33067 6.17523 5.40119 6.34504 5.52673 6.47024L11.0601 11.9821L5.52673 17.494C5.45694 17.5536 5.40026 17.627 5.36025 17.7095C5.32023 17.7919 5.29774 17.8818 5.2942 17.9734C5.29065 18.0649 5.30612 18.1562 5.33964 18.2416C5.37315 18.3269 5.42399 18.4044 5.48896 18.4692C5.55393 18.534 5.63163 18.5847 5.71718 18.6181C5.80273 18.6515 5.89429 18.6669 5.9861 18.6634C6.07791 18.6599 6.168 18.6374 6.25071 18.5975C6.33342 18.5576 6.40697 18.5011 6.46673 18.4315L12.0001 12.9196L17.5267 18.4315C17.6543 18.5404 17.8183 18.5973 17.9861 18.5909C18.1539 18.5844 18.313 18.515 18.4318 18.3966C18.5505 18.2782 18.6201 18.1195 18.6265 17.9521C18.633 17.7848 18.5759 17.6212 18.4667 17.494L12.9401 11.9821Z" fill="#D5B4A8"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_26_867">
+    <rect width="24" height="24" fill="white"/>
+    </clipPath>
+    </defs>
+  </svg>
+  </button>
   <p class="order__item-price">${item.price}&nbsp;₽</p>
   `;
 
@@ -302,7 +316,7 @@ const createCartItem = (item) => {
 
 const renderCart = () => {
   const modalOrder = document.querySelector(".modal_order");
-  const orderCount = modalOrder.querySelector(".order_count");
+  const orderCount = modalOrder.querySelector(".order-count");
   const orderList = modalOrder.querySelector(".order__list");
   const orderTotalPrice = modalOrder.querySelector(".order__total-price");
   const orderForm = modalOrder.querySelector(".order__form");
@@ -320,6 +334,14 @@ const renderCart = () => {
     0
   )} ₽`;
 
+  const orderItemDelete = modalOrder.querySelectorAll(".order__item-delete");
+  orderItemDelete.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+      cartDataControl.remove(item.dataset.idls);
+      renderCart();
+    })
+  })
+
   orderForm.addEventListener("submit", async (event) => {
     event.preventDefault;
     if (!orderListData.length) {
@@ -333,11 +355,9 @@ const renderCart = () => {
           products: orderListData,
         }),
         headers: {
-          "Content-type": "application/json"
-        }
-      })
-      const {message} = await response.json();
-      alert(message);
+          "Content-type": "application/json",
+        },
+      });
       cartDataControl.clear();
       orderForm.reset();
       modalOrder.close("close");
@@ -345,7 +365,24 @@ const renderCart = () => {
   });
 };
 
+const initCountCart = () => {
+  const headerBtnOrder = document.querySelector(".header__btn-order");
+  const span = document.createElement("span");
+
+  const countCart = cartDataControl.get();
+
+  if (countCart.length ) {
+    span.classList.add("header__btn-count");
+    span.innerHTML = countCart.length;
+    headerBtnOrder.append(span);
+  }else if(span){
+    headerBtnOrder.append(span);
+    document.querySelector(".header__btn-order > span").remove();
+  }
+};
+
 const init = async () => {
+  initCountCart();
   modalConroller({
     modal: ".modal_order",
     btnOpen: ".header__btn-order",
