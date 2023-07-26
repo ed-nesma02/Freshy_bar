6078122b9cfe229e0e6a286fd8e2db7f7c5f28b8
ed-nesma02 <shkurko26081978@gmail.com>
2023-07-26 18:18:52 +0300

    change global  API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/"; //https://freshy-bar-api.glitch.me/
+const API_URL = "https://freshy-bar-api.glitch.me/"; //https://freshy-bar-api.glitch.me/
 
 const price = {
   Клубника: 60,
